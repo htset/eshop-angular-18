67fad5c5a2145254b6842cc9e5c0a484de1033b9
Haris Tse <haris.tsetsekas@gmail.com>
2024-08-12 12:33:30 +0300

    part9

--- eshop-angular-18.client/src/app/services/store.service.ts
+++ eshop-angular-18.client/src/app/services/store.service.ts
@@ -3,11 +3,23 @@ import { BehaviorSubject, Subject } from 'rxjs';
 import { Filter } from '../models/filter';
 import { Cart } from '../models/cart';
 import { User } from '../models/user';
+import { Item } from '../models/item';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StoreService {
+
+  private readonly _items = new BehaviorSubject<Item[]>([]);
+  readonly items$ = this._items.asObservable();
+
+  get items(): Item[] {
+    return this._items.getValue();
+  }
+
+  set items(val: Item[]) {
+    this._items.next(val);
+  }
 
   private readonly _page = new BehaviorSubject<number>(1);
   readonly page$ = this._page.asObservable();
@@ -22,7 +34,18 @@ export class StoreService {
 
   public pageSize: number = 3;
   public readonly _pageSizeSubject = new Subject<number>();
-  public pageSizeChanges$ = this._pageSizeSubject.asObservable(); 
+  public pageSizeChanges$ = this._pageSizeSubject.asObservable();
+
+  private readonly _count = new BehaviorSubject<number>(1);
+  readonly count$ = this._count.asObservable();
+
+  get count(): number {
+    return this._count.getValue();
+  }
+
+  set count(val: number) {
+    this._count.next(val);
+  }
 
   private readonly _filter = new BehaviorSubject<Filter>({ name: "", categories: [] });
   readonly filter$ = this._filter.asObservable();
